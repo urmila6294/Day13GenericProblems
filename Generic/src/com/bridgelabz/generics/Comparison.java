4d6924490cac9485629d1279a9cec301f7b5d7a1
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-26 23:06:14 +0530

    Added max float number amoung 3 float numbers

--- Generic/src/com/bridgelabz/generics/Comparison.java
+++ Generic/src/com/bridgelabz/generics/Comparison.java
@@ -1,14 +1,14 @@
 package com.bridgelabz.generics;
 import java.util.Scanner;
 public class Comparison {
-    public static void max_int() {
+    public static void maximumInteger() {
         Integer num1, num2, num3;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Number:");
+        System.out.println("Enter First Integer Number:");
         num1 = sc.nextInt();
-        System.out.println("Enter Second Number:");
+        System.out.println("Enter Second Integer Number:");
         num2 = sc.nextInt();
-        System.out.println("Enter Third Number:");
+        System.out.println("Enter Third Integer Number:");
         num3 = sc.nextInt();
         Integer max = num1;
         if (num2.compareTo(max) > 0)
@@ -16,7 +16,24 @@ public class Comparison {
         if (num3.compareTo(max) > 0)
             max = num3;
 
-        System.out.println("Maximum number:" + max);
+        System.out.println("Maximum Integer Number Is:" + max);
+    }
+    public static void maximumFloat() {
+        Float num1, num2, num3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Float Number:");
+        num1 = sc.nextFloat();
+        System.out.println("Enter Second Float Number:");
+        num2 = sc.nextFloat();
+        System.out.println("Enter Third Float Number:");
+        num3 = sc.nextFloat();
+        Float max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
+
+        System.out.println("Maximum Float Number:" + max);
     }
 }
 
