47f3b4407fef55f194f52e8b349ef0cf522b5639
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-26 23:36:28 +0530

    Added find max amoung variables using generic method passing passing parameters

--- Generic/src/com/bridgelabz/generics/FindMax.java
+++ Generic/src/com/bridgelabz/generics/FindMax.java
@@ -1,31 +1,41 @@
 package com.bridgelabz.generics;
+public class FindMax<T extends Comparable<T>> {
+    T input1, input2, input3;
 
-public class FindMax {
-        public static <T extends Comparable<T>> T getMax(T num1, T num2, T num3) { // Refactor all 3 in one Generic Method
-            T max = num1;
-            if (num2.compareTo(max) > 0)
-                max = num2;
-            if (num3.compareTo(max) > 0)
-                max = num3;
-            showMaxValue(num1, num2, num3, max);
-            return max;
-        }
+    public FindMax(T input1, T input2, T input3) {
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+    }
 
-        private static <T extends Comparable<T>> void showMaxValue(T num1, T num2, T num3, T max) {
-            System.out.println("the input1 is " + num1 + "\ninput2 is " + num2 + "\ninput3 is " + num3);
-            System.out.println("Maximum : " + max + "\n");
-        }
+    public T testMaximum() {
+        return FindMax.getMax(input1, input2, input3);
+    }
 
-        public static void main(String[] args) {
-            Integer intValue1 = 65, intValue2 = 17, intValue3 = 8954;
-            Float floatValue1 = 00.2f, floatValue2 = 23.56f, floatValue3 = 0.00157f;
-            String stringValue1 = "Urmila", stringValue2 = "RakeshKumar", stringValue3 = "Sheral";
+    public static <T extends Comparable<T>> T getMax(T num1, T num2, T num3) { // Refactor all 3 in one Generic Method
+        T max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
+        showMaxValue(num1, num2, num3, max);
+        return max;
+    }
 
-            FindMax maximum = new FindMax();
-            maximum.getMax(intValue1, intValue2, intValue3);
-            maximum.getMax(floatValue1, floatValue2, floatValue3);
-            maximum.getMax(stringValue1, stringValue2, stringValue3);
+    private static <T extends Comparable<T>> void showMaxValue(T num1, T num2, T num3, T max) {
+        System.out.println("the input1 is " + num1 + "\ninput2 is " + num2 + "\ninput3 is " + num3);
+        System.out.println("Maximum : " + max + "\n");
+    }
 
-        }
+    public static void main(String[] args) {
+        Integer intValue1 = 300, intValue2 = 900, intValue3 = 60;
+        Float floatValue1 = 9.3f, floatValue2 = 3.5f, floatValue3 = 8.7f;
+        String stringValue1 = "Arjun", stringValue2 = "Rakesh", stringValue3 = "Sheral";
+
+        new FindMax<>(intValue1, intValue2, intValue3).testMaximum();
+        new FindMax<>(floatValue1, floatValue2, floatValue3).testMaximum();
+        new FindMax<>(stringValue1, stringValue2, stringValue3).testMaximum();
+    }
+
 }
 
