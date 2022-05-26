00878170c8af4f6a65d69187fa0f8a61b8278e55
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-26 22:58:10 +0530

    Added max integer number amoung 3 integer numbers

---
+++ Generic/src/com/bridgelabz/generics/FindMax.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.generics;
+
+public class FindMax {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Program For Finding Maximum Number Among Three");
+        Comparison compare = new Comparison();
+        compare.max_int();
+
+    }
+
+}
+
